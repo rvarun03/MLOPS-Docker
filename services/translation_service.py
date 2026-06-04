bdda54a0438a64a7ed4f37264f0ec45c86b53772
varun <vivabrothers9703@gmail.com>
2026-06-04 11:46:33 +0530

    Added Google Translator feature

--- services/translation_service.py
+++ services/translation_service.py
@@ -1,6 +1,12 @@
+from deep_translator import GoogleTranslator
+
 def translate_text(text: str, target_language: str):
+    translated=GoogleTranslator(
+        source='auto',
+        target=target_language
+    ).translate(text)
     return {
         "original_text": text,
         "target_language": target_language,
-        "translated_text": f"Dummy translation of '{text}'"
+        "translated_text": translated 
     }
